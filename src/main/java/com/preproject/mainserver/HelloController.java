8f0d4af18a249be5dd5432cd53c7595834607da5
JanuaryKim <unool6976@gmail.com>
2022-10-23 01:49:21 +0900

    Feat: 기본 베이스 프로젝트 생성
    헬로 컨트롤러 생성
    
    완성

---
+++ src/main/java/com/preproject/mainserver/HelloController.java
@@ -0,0 +1,18 @@
+package com.preproject.mainserver;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/")
+    public ResponseEntity hello() {
+
+        return new ResponseEntity("Hello!", HttpStatus.OK);
+    }
+}
