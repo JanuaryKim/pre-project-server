432ba51b1dd1cd22bc5034386156bb1d3b72c21d
JanuaryKim <unool6976@gmail.com>
2022-10-25 04:03:38 +0900

    feat: 테스트 수정

--- src/test/java/com/preproject/mainserver/MainServerApplicationTests.java
+++ src/test/java/com/preproject/mainserver/MainServerApplicationTests.java
@@ -3,11 +3,11 @@ package com.preproject.mainserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MainServerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class MainServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
